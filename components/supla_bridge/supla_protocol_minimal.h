b1323f070225423b720bdaf0ce89750b0b1fa47d
stokiloton <stokiloton@gmail.com>
2026-01-08 21:21:19 +0100

    Update supla_protocol_minimal.h

--- components/supla_bridge/supla_protocol_minimal.h
+++ components/supla_bridge/supla_protocol_minimal.h
@@ -1,7 +1,17 @@
 #pragma once
-#include <Arduino.h>
+#include <stdint.h>
 
 #pragma pack(push, 1)
+
+// Typy pakietów (uproszczone ID)
+enum SuplaPacketType : uint16_t {
+  SUPLA_SD_DEVICE_REGISTER       = 10,
+  SUPLA_SD_DEVICE_REGISTER_RESULT= 20,
+  SUPLA_SD_SET_VALUE             = 30,
+  SUPLA_SD_VALUE_CHANGED         = 40,
+  SUPLA_SD_PING_SERVER           = 50,
+  SUPLA_SD_PING_CLIENT           = 51,
+};
 
 enum SuplaChannelType : uint8_t {
   SUPLA_CHANNELTYPE_SENSOR_TEMP = 10,
@@ -17,32 +27,46 @@ struct SuplaChannelConfig {
   SuplaChannelType type;
 };
 
-struct SuplaRegisterDevicePacket {
-  uint8_t version;
+struct SuplaDeviceRegister {
+  uint16_t type;           // SUPLA_SD_DEVICE_REGISTER
+  uint8_t proto_version;   // np. 1
+  SuplaGuid guid;
   uint32_t location_id;
   uint8_t location_password[16];
-  SuplaGuid guid;
   char device_name[32];
   uint8_t channel_count;
   SuplaChannelConfig channels[2];  // 0: temp, 1: relay
 };
 
-struct SuplaTemperatureUpdate {
-  uint8_t type;          // 1 = temp update
+struct SuplaDeviceRegisterResult {
+  uint16_t type;           // SUPLA_SD_DEVICE_REGISTER_RESULT
+  uint8_t result;          // 0 = OK
+  uint32_t device_id;
+};
+
+struct SuplaValueChangedTemp {
+  uint16_t type;           // SUPLA_SD_VALUE_CHANGED
   uint8_t channel_number;
+  uint8_t value_type;      // 1 = temp
   float temperature;
 };
 
-struct SuplaRelayUpdate {
-  uint8_t type;          // 2 = relay update
+struct SuplaValueChangedRelay {
+  uint16_t type;           // SUPLA_SD_VALUE_CHANGED
   uint8_t channel_number;
-  uint8_t state;         // 0/1
+  uint8_t value_type;      // 2 = relay
+  uint8_t state;           // 0/1
 };
 
-struct SuplaRelayCommand {
-  uint8_t type;          // 3 = relay command
+struct SuplaSetValueRelay {
+  uint16_t type;           // SUPLA_SD_SET_VALUE
   uint8_t channel_number;
-  uint8_t state;         // 0/1
+  uint8_t value_type;      // 2 = relay
+  uint8_t state;           // 0/1
+};
+
+struct SuplaPing {
+  uint16_t type;           // SUPLA_SD_PING_SERVER lub SUPLA_SD_PING_CLIENT
 };
 
 #pragma pack(pop)
