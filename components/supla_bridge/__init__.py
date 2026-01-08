0234e335f04ab14b6fef802ba1008a3c46a1dc71
stokiloton <stokiloton@gmail.com>
2026-01-08 20:37:27 +0100

    Update __init__.py

--- components/supla_bridge/__init__.py
+++ components/supla_bridge/__init__.py
@@ -1,60 +1,41 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
+from esphome.components import sensor, light
 from esphome.const import CONF_ID
 
-from esphome import automation
-
-supla_bridge_ns = cg.esphome_ns.namespace("supla_bridge")
-SuplaBridge = supla_bridge_ns.class_("SuplaBridge", cg.Component)
-
 CONF_SERVER = "server"
-CONF_USER = "user"
-CONF_PASSWORD = "password"
+CONF_LOCATION_ID = "location_id"
+CONF_LOCATION_PASSWORD = "location_password"
+CONF_DEVICE_NAME = "device_name"
+CONF_TEMPERATURE = "temperature"
+CONF_SWITCH = "switch"
 
-CONF_ON_SUPLA_TURN_ON = "on_supla_turn_on"
-CONF_ON_SUPLA_TURN_OFF = "on_supla_turn_off"
+supla_ns = cg.esphome_ns.namespace("supla_esphome_bridge")
+SuplaEsphomeBridge = supla_ns.class_("SuplaEsphomeBridge", cg.Component)
 
 CONFIG_SCHEMA = cv.Schema(
     {
-        cv.GenerateID(): cv.declare_id(SuplaBridge),
+        cv.GenerateID(): cv.declare_id(SuplaEsphomeBridge),
         cv.Required(CONF_SERVER): cv.string,
-        cv.Required(CONF_USER): cv.string,
-        cv.Required(CONF_PASSWORD): cv.string,
-
-        cv.Optional(CONF_ON_SUPLA_TURN_ON): automation.validate_automation(
-            cv.Schema({})
-        ),
-        cv.Optional(CONF_ON_SUPLA_TURN_OFF): automation.validate_automation(
-            cv.Schema({})
-        ),
+        cv.Required(CONF_LOCATION_ID): cv.int_,
+        cv.Required(CONF_LOCATION_PASSWORD): cv.string,
+        cv.Optional(CONF_DEVICE_NAME, default="esphome"): cv.string,
+        cv.Required(CONF_TEMPERATURE): cv.use_id(sensor.Sensor),
+        cv.Required(CONF_SWITCH): cv.use_id(light.LightState),
     }
-).extend(cv.COMPONENT_SCHEMA)
-
+)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
     cg.add(var.set_server(config[CONF_SERVER]))
-    cg.add(var.set_email(config[CONF_USER]))
-    cg.add(var.set_password(config[CONF_PASSWORD]))
+    cg.add(var.set_location_id(config[CONF_LOCATION_ID]))
+    cg.add(var.set_location_password(config[CONF_LOCATION_PASSWORD]))
+    cg.add(var.set_device_name(config[CONF_DEVICE_NAME]))
 
-    # Triggery: SUPLA -> ESPHome
+    temp = await cg.get_variable(config[CONF_TEMPERATURE])
+    sw = await cg.get_variable(config[CONF_SWITCH])
 
-    # on_supla_turn_on: lista automatyzacji → iterujemy
-    for conf in config.get(CONF_ON_SUPLA_TURN_ON, []):
-        trig = var.get_turn_on_trigger()
-        await automation.build_automation(
-            trig,
-            [],
-            conf,
-        )
-
-    # on_supla_turn_off: lista automatyzacji → iterujemy
-    for conf in config.get(CONF_ON_SUPLA_TURN_OFF, []):
-        trig = var.get_turn_off_trigger()
-        await automation.build_automation(
-            trig,
-            [],
-            conf,
-        )
+    cg.add(var.set_temperature_sensor(temp))
+    cg.add(var.set_switch_light(sw))
