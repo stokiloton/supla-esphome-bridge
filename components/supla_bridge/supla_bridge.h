8a5af5b102b444070fbacda988a76126af995c36
stokiloton <stokiloton@gmail.com>
2026-01-08 14:13:29 +0100

    Update supla_bridge.h

--- components/supla_bridge/supla_bridge.h
+++ components/supla_bridge/supla_bridge.h
@@ -1,7 +1,11 @@
 #pragma once
+
 #include "esphome/core/component.h"
+#include "esphome/core/automation.h"
 #include "esphome/core/helpers.h"
+
 #include <SuplaDevice.h>
+#include <supla/control/virtual_relay.h>
 
 namespace esphome {
 namespace supla_bridge {
@@ -11,28 +15,36 @@ class SuplaBridge : public Component {
   std::string server_;
   std::string email_;
   std::string password_;
-  bool switch_state_ = false;
-  std::function<void(bool)> switch_callback_;
+
+  Supla::Control::VirtualRelay *relay_{nullptr};
+
+  Trigger<> *on_turn_on_trigger_{nullptr};
+  Trigger<> *on_turn_off_trigger_{nullptr};
 
   void set_server(const std::string &server) { server_ = server; }
   void set_email(const std::string &email) { email_ = email; }
   void set_password(const std::string &password) { password_ = password; }
 
-  void set_switch_callback(std::function<void(bool)> cb) {
-    switch_callback_ = cb;
-  }
+  void set_on_turn_on_trigger(Trigger<> *tr) { on_turn_on_trigger_ = tr; }
+  void set_on_turn_off_trigger(Trigger<> *tr) { on_turn_off_trigger_ = tr; }
 
   void setup() override {
-    SuplaDevice.setName("ESPHome Bridge");
+    // Nazwa urządzenia w SUPLI
+    SuplaDevice.setName("ESPHome SUPLA Bridge");
 
-    SuplaDevice.addRelay([](int channel_number, bool on) {
-      auto *bridge = (SuplaBridge *) SuplaDevice.getUserContext();
-      if (bridge && bridge->switch_callback_) {
-        bridge->switch_callback_(on);
+    // Wirtualny przekaźnik (bez fizycznego pinu)
+    relay_ = new Supla::Control::VirtualRelay();
+
+    // Zmiana stanu z SUPLA -> ESPHome
+    relay_->setOnChangeCallback([this](bool on) {
+      if (on && this->on_turn_on_trigger_ != nullptr) {
+        this->on_turn_on_trigger_->trigger();
+      } else if (!on && this->on_turn_off_trigger_ != nullptr) {
+        this->on_turn_off_trigger_->trigger();
       }
     });
 
-    SuplaDevice.setUserContext(this);
+    SuplaDevice.addChannel(relay_);
     SuplaDevice.begin(email_.c_str(), password_.c_str(), server_.c_str());
   }
 
@@ -40,12 +52,13 @@ class SuplaBridge : public Component {
     SuplaDevice.iterate();
   }
 
+  // ESPHome -> SUPLA
   void update_switch(bool state) {
-    switch_state_ = state;
-    SuplaDevice.channelValueChanged(0, state);
+    if (relay_ != nullptr) {
+      relay_->setState(state);
+    }
   }
 };
 
 }  // namespace supla_bridge
 }  // namespace esphome
-
