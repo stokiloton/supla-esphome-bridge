4b985d568d2b8f6204ba62a695ca46d761a83550
stokiloton <stokiloton@gmail.com>
2026-01-09 16:28:30 +0100

    Update supla_suml.h

--- components/supla_bridge/supla_suml.h
+++ components/supla_bridge/supla_suml.h
@@ -38,4 +38,32 @@ typedef struct {
 // ------------------------------------------------------------
 
 static inline uint16_t supla_crc16(const uint8_t *data, uint16_t len) {
-    uint
+    uint16_t crc = 0xFFFF;
+
+    for (uint16_t i = 0; i < len; i++) {
+        crc ^= data[i];
+        for (uint8_t j = 0; j < 8; j++) {
+            if (crc & 1)
+                crc = (crc >> 1) ^ 0xA001;
+            else
+                crc >>= 1;
+        }
+    }
+
+    return crc;
+}
+
+// ------------------------------------------------------------
+// Przygotowanie nagłówka SUM-L
+// ------------------------------------------------------------
+
+static inline void supla_prepare_header(SuplaPacketHeader &hdr,
+                                        uint16_t size,
+                                        const uint8_t *payload) {
+    hdr.marker[0] = 'S';
+    hdr.marker[1] = 'U';
+    hdr.marker[2] = 'P';
+    hdr.version   = 1;
+    hdr.data_size = size;
+    hdr.crc16     = supla_crc16(payload, size);
+}
