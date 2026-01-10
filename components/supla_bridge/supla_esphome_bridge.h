6724ad06ac1e7b5e0e563f8ad77e429bd76a0984
stokiloton <stokiloton@gmail.com>
2026-01-10 13:35:33 +0100

    Update supla_esphome_bridge.h

--- components/supla_bridge/supla_esphome_bridge.h
+++ components/supla_bridge/supla_esphome_bridge.h
@@ -7,13 +7,14 @@
 #include <string>
 
 #include "proto.h"
+#include "srpc.h"
 
 namespace supla_esphome_bridge {
 
 class SuplaEsphomeBridge : public esphome::Component {
  public:
   SuplaEsphomeBridge();
-  ~SuplaEsphomeBridge() override;
+  ~SuplaEsphomeBridge();  // <--- usunięte 'override' (kompilator ESPHome wymaga takiej deklaracji)
 
   // ESPHome setters (zgodne z __init__.py)
   void set_server(const std::string &server) { server_ = server; }
@@ -52,11 +53,11 @@ class SuplaEsphomeBridge : public esphome::Component {
   esphome::sensor::Sensor *temperature_sensor_{nullptr};
   esphome::light::LightState *switch_light_{nullptr};
 
-  // sproto context
+  // sproto/srpc context
   void *sproto_ctx_{nullptr};
 
   // stały GUID (binarnie) - 1C81FE5A-DDDD-BCD1-FCC1-0F42C159618E
-  static const uint8_t GUID_BIN[SUPLA_GUID_SIZE];
+  static const uint8_t GUID_BIN[16];
 };
 
 }  // namespace supla_esphome_bridge
