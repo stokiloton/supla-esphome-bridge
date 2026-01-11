a3923ac28338b41d46f45f550d6a4c9a85adf361
stokiloton <stokiloton@gmail.com>
2026-01-11 16:59:40 +0100

    Update supla_esphome_bridge.cpp

--- components/supla_bridge/supla_esphome_bridge.cpp
+++ components/supla_bridge/supla_esphome_bridge.cpp
@@ -121,10 +121,7 @@ bool SuplaEsphomeBridge::register_device(unsigned long timeout_ms) {
   // ServerName (musi odpowiadać temu, co wpisujesz jako server)
   strncpy(reg.ServerName, server_.c_str(), SUPLA_SERVER_NAME_MAXSIZE - 1);
 
-  // Flags, ManufacturerID, ProductID
-  reg.Flags = 0;
-  reg.ManufacturerID = 0;
-  reg.ProductID = 0;
+
   
 
 
