c068772f5abff739e1c2bd473548034e7fc686c9
stokiloton <stokiloton@gmail.com>
2026-01-11 17:18:11 +0100

    Update supla_esphome_bridge.cpp

--- components/supla_bridge/supla_esphome_bridge.cpp
+++ components/supla_bridge/supla_esphome_bridge.cpp
@@ -111,7 +111,7 @@ bool SuplaEsphomeBridge::register_device(unsigned long timeout_ms) {
 
 
  // SoftVer
-  strncpy(reg.SoftVer, "GG 1.0", SUPLA_SOFTVER_MAXSIZE - 1);
+  strncpy(reg.SoftVer, device_name_.c_str(), SUPLA_SOFTVER_MAXSIZE - 1);
 
   // ServerName (musi odpowiadać temu, co wpisujesz jako server)
   strncpy(reg.ServerName, server_.c_str(), SUPLA_SERVER_NAME_MAXSIZE - 1);
